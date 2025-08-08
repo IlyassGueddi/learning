436752d43155dfd528636594a5b29805103c9937
ilyassgueddi <gueddiilyass99@gmail.com>
2025-08-08 18:37:34 +0100

    first

---
+++ range.py
@@ -0,0 +1,23 @@
+#we write range like that: range(start, end, step)
+
+#print the numbers from 1 to 10
+'''for i in range(1, 11):
+  print(i)'''
+
+#print the number from 1 to 10 lik that (1 - 3 - 5- ....)
+'''for i in range(1, 11, 2):
+  print(i)'''
+
+# write the everything from 0 to 5
+'''for i in range(5):
+  print(i)'''
+
+# orint hello 10 times
+'''for i in range(10):
+    print("Hello")'''
+
+# this code will show the name number and list and in his site the name 
+# name[i] show the name who have the number i
+'''names = ["Alice", "Bob", "Mial"]
+for i in range(len(names)):
+    print(i, names[i])'''
